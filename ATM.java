bafdc32cb32a6b94bebdb51d6c97af4c3970b245
PENUMAKA RUPAS BABU <107531957+RUPAS13788@users.noreply.github.com>
2024-12-26 17:52:20 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -83,7 +83,7 @@ class ATM {
 public class Atm
  {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(10000.00); // Initial balance
+        BankAccount account = new BankAccount(10000.00);
         ATM atm = new ATM(account);
         atm.run();
     }
